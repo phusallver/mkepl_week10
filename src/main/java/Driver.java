b042b95273875d5074abf7aa6aef75d79f3823e9
phusallver <103794258+phusallver@users.noreply.github.com>
2024-05-12 09:41:00 +0700

    coba trigger push ke main

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -14,6 +14,7 @@ public class Driver {
 		System.out.println("Count:" + counter.getCount());
 		
 		counter.decrement();
+		counter.decrement();
 		
 		System.out.println("Count:" + counter.getCount());
 		
